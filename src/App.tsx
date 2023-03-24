d5c168f4dd2c922cce58724d7401875c351e5962
剑舞幽若 <609228186@qq.com>
2023-03-24 16:22:00 +0800

    初始化仓库

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div className="container">
+      <h1>测试页面</h1>
+      {/* TODO 倒计时 */}
+    </div>
+  );
+}
+
+export default App;
