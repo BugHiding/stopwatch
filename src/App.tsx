907e2aabd632d8c8f30fbe115f5c0772c03c4f20
剑舞幽若 <609228186@qq.com>
2023-03-27 11:00:38 +0800

    添加清零，开始，停止按钮

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,54 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import React from "react";
+
+const addZero = (num: number) => {
+  return num < 10 ? `0${num}` : num;
+};
+
+const formatTime = (time: number) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  return `${addZero(minutes)}:${addZero(seconds)}`;
+};
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [time, setTime] = React.useState(0);
+  const [timerId, setTimeId] = React.useState<any>(null);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  React.useEffect(() => {
+    return () => clearInterval(timerId);
+  }, [timerId]);
+
+  const handleClear = () => {
+    setTime(0);
+  };
+
+  const handleStart = () => {
+    const id = setInterval(() => {
+      setTime((time) => time + 1);
+    }, 1000);
+
+    setTimeId(id);
+  };
+
+  const handleEnd = () => {
+    if (timerId) clearInterval(timerId);
+  };
 
   return (
     <div className="container">
-      <h1>测试页面</h1>
-      {/* TODO 倒计时 */}
+      <div className="stopwatch">
+        <div className="time-wrapper">
+          <div className="time-back">88:88</div>
+          <div className="time">{formatTime(time)}</div>
+        </div>
+      </div>
+
+      <div className="action">
+        <button onClick={handleClear}>清零</button>
+        <button onClick={handleStart}>开始</button>
+        <button onClick={handleEnd}>停止</button>
+      </div>
     </div>
   );
 }
